ae2819fdb4da08e02ba114737365e67b5d42a61a
Lilycoeur972 <delivertl@gmail.com>
2022-03-22 21:26:02 +0100

    portfolio

--- js/script.js
+++ js/script.js
@@ -13,11 +13,11 @@ window.onscroll= () =>{
     header.classList.remove('active')
 }
 let cursor1 = document.querySelector('.cursor-1');
-let cursor2 = document.querySelector('.curor-2');
+let cursor2 = document.querySelector('.cursor-2');
 
 window.onmousemove = (e) =>{
     cursor1.style.top = e.pageY +'px';
-    cursor2.style.left = e.pageX +'px';
+    cursor1.style.left = e.pageX +'px';
     cursor2.style.top = e.pageY + 'px';
     cursor2.style.left = e.pageX + 'px';   
 }
